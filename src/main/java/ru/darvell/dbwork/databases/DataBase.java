df113849dd679b834d40d3354f68e6bffa34f163
andrewdarvell <andrew@darvell.ru>
2014-10-07 15:17:08 +0400

    Немного JavaDoc

--- src/main/java/ru/darvell/dbwork/databases/DataBase.java
+++ src/main/java/ru/darvell/dbwork/databases/DataBase.java
@@ -3,7 +3,20 @@ package ru.darvell.dbwork.databases;
 import java.sql.PreparedStatement;
 import java.util.Map;
 
+/**
+ * Класс выполнет всю основную работу с бд
+ */
+
 public interface DataBase {
+	/**
+	 * <p>Уставнавливает параметры для работы с БД</p>
+	 * @param dbName название базы данных
+	 * @param login имя пользователя
+	 * @param pass пароль
+	 * @param host хост
+	 * @param port порт
+	 * @return true в случае успеха
+	 */
 	Boolean setParameters(String dbName, String login, String pass, String host, String port);
 	Boolean setParametersFromConf(Map<String,String> parameters);
 	Boolean connect();
